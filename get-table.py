c9cf8082042d39c2e0199e1a8587f036dc555e9a
Brendan Lauck <v-brlauc@microsoft.com>
2019-05-23 16:13:20 -0700

    Added functionality to handle .pdf with one to many pages with different tables

--- get-table.py
+++ get-table.py
@@ -1,113 +1,27 @@
-# Gets a single table from a jpeg and writes to an excel file
+__title__ = "Table Image Extractor"
+__author__ = "Brendan Lauck"
+__maintainer__ = "Brendan Lauck"
+__email__ = "brendanlauck@gmail.com"
+__status__ = "development"
+
 import cv2 as cv
+import main
 import numpy as np
 import sys
-import xlsxwriter
 import utils
 
 
 if __name__ == "__main__":
-    # get arguments
+    """PROCESS INPUT, LOAD IMAGE, AND GET TABLE FROM IMAGE"""
     if len(sys.argv) == 2:
         imagePath = sys.argv[1]
+        if imagePath[-4:] == ".pdf":
+            numPages = utils.pdfToJpg(imagePath)
+            for pageNum in range(1, numPages + 1):
+                img = cv.imread("out{}.jpg".format(pageNum))
+                main.getTable(img, pageNum)
+        elif imagePath[-4:] == ".jpg":
+            main.getTable(cv.imread(imagePath), 1)
     else:
         print("Error: Usage: python get-table.py <path to image>")
-        sys.exit(0)
-
-
-
-    ###################################
-    # LOAD IMAGE
-    ###################################
-    original_image = cv.imread(imagePath)
-
-
-
-    ###################################
-    # CONVERT COLORSPACE TO NEGATIVE
-    ###################################
-    gray_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
-
-
-
-    ###################################
-    # APPLY ADAPTIVE THRESHOLD
-    ###################################
-    gray_image = cv.bitwise_not(gray_image)
-    _, image_withThreshold = cv.threshold(gray_image,127,255,cv.THRESH_BINARY)
-
-
-
-    ###################################
-    # EXTRACT TABLE LINES
-    ###################################
-    horizontal_lines = np.copy(image_withThreshold)
-    vertical_lines = np.copy(image_withThreshold)
-
-    # get horizontal lines
-    cols = horizontal_lines.shape[1]
-    horizontal_lines_size = cols // 30
-
-    horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (horizontal_lines_size, 1))
-
-    horizontal_lines = cv.erode(horizontal_lines, horizontal_kernel)
-    horizontal_lines = cv.dilate(horizontal_lines, horizontal_kernel)
-
-    # get vertical lines
-    rows = vertical_lines.shape[0]
-    vertical_lines_size = rows // 30
-
-    vertical_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, vertical_lines_size))
     
-    vertical_lines = cv.erode(vertical_lines, vertical_kernel)    
-    vertical_lines = cv.dilate(vertical_lines, vertical_kernel)
-
-
-
-    ###################################
-    # CREATE LINE MASK AND FIND CONTOURS
-    ################################### 
-    line_mask = horizontal_lines + vertical_lines
-
-    (contours, _) = cv.findContours(line_mask, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-
-
-
-    ###################################
-    # EXTRACT TABLE AND OCR
-    ###################################
-    x_bounds = set()
-    y_bounds = set()
-    numRows = 0
-    numCols = 0
-    texts = []
-
-    for i in range(len(contours) - 2, -1, -1):
-        x, y, w, h = cv.boundingRect(contours[i]) # get contour coordinates
-        x += 1; w -= 2 # offset HARD CODING, LOL YIKES
-        box = original_image[y:y + h, x:x + w] # get bounding box
-        texts.append(utils.run_tesseract(box, 8, 3)) # extract text from bounding box with Tesseract
-
-        # use bounding box coordinates to track num rows and num cols
-        if x not in x_bounds:
-            x_bounds.add(x)
-            numCols += 1
-        if y not in y_bounds:
-            y_bounds.add(y)
-            numRows += 1
-
-
-
-    ###################################
-    # WRITE TABLE TO EXCEL
-    ###################################
-    workbook = xlsxwriter.Workbook('test_table.xlsx')
-    worksheet = workbook.add_worksheet('table from image')
-
-    text_index = 0
-    for row in range(numRows): # double for loop, sue me
-        for col in range(numCols):
-            worksheet.write(row, col, texts[text_index])
-            text_index += 1
-
-    workbook.close()
--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
 
 import cv2 as cv
+import numpy as np
+from pdf2image import convert_from_path
 import pytesseract as tess
-import os
 
-# runs tesseract to perform
+
+
 def run_tesseract(image, psm, oem):
+    """RUNS TESSERACT TO PERFORM OCR"""
     language = 'eng'
     configuration = "--psm " + str(psm) + " --oem " + str(oem)
 
@@ -18,28 +21,74 @@ def run_tesseract(image, psm, oem):
 
 
 
-# show image FOR DEBUGGING
+def extractTableLines(image):
+    """EXTRACTS HORIZONTAL AND VERTICAL TABLE LINES FROM AN IMAGE"""
+    horizontal_lines = np.copy(image)
+    vertical_lines = np.copy(image)
+
+    # get horizontal lines
+    cols = horizontal_lines.shape[1]
+    horizontal_lines_size = cols // 30
+
+    horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (horizontal_lines_size, 1))
+
+    horizontal_lines = cv.erode(horizontal_lines, horizontal_kernel)
+    horizontal_lines = cv.dilate(horizontal_lines, horizontal_kernel)
+
+    # get vertical lines
+    rows = vertical_lines.shape[0]
+    vertical_lines_size = rows // 30
+
+    vertical_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, vertical_lines_size))
+    
+    vertical_lines = cv.erode(vertical_lines, vertical_kernel)    
+    vertical_lines = cv.dilate(vertical_lines, vertical_kernel)
+
+    return horizontal_lines, vertical_lines
+
+
+
 def showImage(image, title):
+    """SHOW IMAGE FOR DEBUGGING"""
     cv.imshow(title, image)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
-# show contours FOR DEBUGGING
+
+
 def showContours(contours):
+    """SHOW CONTOURS FOR DEBUGGING"""
     contours_image = np.zeros((720,1280,3), np.uint8)
     cv.drawContours(contours_image, contours, -1, (0,255,0), 2)
     showImage(contours_image, "contours")
 
-# show contour on image FOR DEBUGGING
+
+
 def showContoursOnImage(contours, image):
+    """SHOW CONTOURS ITERATIVELY ON IMAGE FOR DEBUGGING"""
     for i in range(len(contours) - 1, -1, -1):
         img = image
         cv.drawContours(img, contours, i, (0, 0, 255), 1)
         showImage(img, "image with contour")
 
-# show contours iteratively on a black image FOR DEBUGGING
+
+
 def showContoursIter(contours):
+    """SHOW CONTOURS ITERATIVELY ON A BLACK CANVAS FOR DEBUGGING"""
     contours_image = np.zeros((720,1280,3), np.uint8)
     for i in range(len(contours) - 1, -1, -1):
         cv.drawContours(contours_image, contours, i, (0,255,0), 2)
         showImage(contours_image, "Contours")
+
+
+
+# convert pdf to image
+def pdfToJpg(path):
+    """CONVERT PDF TO IMAGE, SAVE, AND RETURN NUMBER OF PAGES"""
+    pages = convert_from_path(path, 500)
+    pageNum = 1
+    for page in pages:
+        page.save("out{}.jpg".format(pageNum), "JPEG")
+        pageNum += 1
+    
+    return len(pages)
